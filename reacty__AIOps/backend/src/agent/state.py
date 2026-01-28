1fe708408ef547e48bac1af4344433d1d30caca5
Gunjan Kumari <gunjankumari12082004@gmail.com>
2026-01-28 11:09:12 +0530

    Add ReflectionState and Query classes to state.py

--- reacty__AIOps/backend/src/agent/state.py
+++ reacty__AIOps/backend/src/agent/state.py
@@ -20,3 +20,29 @@ class OverallState(TypedDict):
     initial_search_query_count: int 
     max_research_loops: int 
     reasoing_model: str 
+
+class ReflectionState(TypedDict):
+    is_sufficient: bool
+    knowledge_gap: str
+    follow_up_queries: Annotated[list, operator.add]
+    research_loop_count: int
+    number_of_ran_queries: int
+
+
+class Query(TypedDict):
+    query: str
+    rationale: str
+
+
+class QueryGenerationState(TypedDict):
+    search_query: list[Query]
+
+
+class WebSearchState(TypedDict):
+    search_query: str
+    id: str
+
+
+@dataclass(kw_only=True)
+class SearchStateOutput:
+    running_summary: str = field(default=None)  # Final report
